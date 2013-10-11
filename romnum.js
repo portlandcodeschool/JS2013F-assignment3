7829f4c5c6259e65f0a252f58cd2268d107f7b2b
David Manning <dlmanning@me.com>
2013-10-11 11:24:43 -0700

    added instructions and setup for assignment 3

---
+++ romnum.js
@@ -0,0 +1,35 @@
+if(!module.parent) { // This is here for me use. Don't worry about it.
+
+// Put any code you need to test your functions in here
+
+// Example:
+console.log("53 in roman numerals is: " + decimalToRoman(53));  
+
+}
+/*-----------------------------------------------------------------------------
+     
+
+     Nothing needs to be in this area
+
+
+-----------------------------------------------------------------------------*/
+function decimalToRoman (decNumber) {
+  var romNumeral = "";
+
+  // Implement here!
+
+  return romNumeral;
+}
+
+function romanToDecimal (romNumeral) {
+  var decNumber = 0;
+
+  // Implement here!
+
+  return decNumber;
+}
+
+// Don't worry about this stuff. It's here so I can more easily test your code.
+
+module.exports.decimalToRoman = decimalToRoman;
+module.exports.romanToDecimal = romanToDecimal;
