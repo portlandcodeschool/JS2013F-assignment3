a9c7a6d75bdbb571d521860b9bc8bf470329c9c7
David Manning <dlmanning@me.com>
2013-10-11 23:16:26 -0700

    instructor comments

--- Fingert/f2c-fingert.js
+++ Fingert/f2c-fingert.js
@@ -1,20 +1,14 @@
 var temperature = 86;
 
-// You shouldn't need any of these variables
-// Even if you did, they shouldn't be strings
-
-
-
-
 console.log(temperature + " degrees Fahrenheit is " +
             fahrenheitToCelsius(temperature) + " degrees Celsius");
 
 console.log(temperature + " degrees Celsius is " + 
             celsiusToFahrenheit(temperature) + " degrees Fahrenheit");
 
-console.log(fahrenheitToCelsius(temperature = 95) + " degrees Celsius is a pretty hot day!");
+console.log(fahrenheitToCelsius(temperature) + " degrees Celsius is a pretty hot day!");
 
-console.log("Water boils at " + celsiusToFahrenheit(100) + " degrees Fahrenheit."); //Doesn't ever return the right value. But why? Please to tell me.
+console.log("Water boils at " + celsiusToFahrenheit(100) + " degrees Fahrenheit.");
 
 
 /* What? How can I call the functions above when they're not defined until below?
@@ -23,10 +17,11 @@ console.log("Water boils at " + celsiusToFahrenheit(100) + " degrees Fahrenheit.
    can make a convention of defining our functions at the end of current scope. */
 
 function celsiusToFahrenheit () {
-  // You should not use a global variable for the function return value
-  // You don't really need a variable at all here. Just return the value of the expression.
+
   return (temperature * 9) / 5 + 32;
-  
+  // the problem here is that 'temperature' is a global variable
+  // you need to be passing in a parameter value to convert
+  // instead of referencing 'temperature' within the function
 }
 
 function fahrenheitToCelsius () {
@@ -34,8 +29,3 @@ function fahrenheitToCelsius () {
   return ((temperature - 32) * 5) / 9;
   
 }
-
-/*function boilingWater () { // This function is unnecessary. Use celsiusToFahrenheit(100) instead.
-  x = (100 * 9) / 5 + 32;
-  return x;
-}*/
