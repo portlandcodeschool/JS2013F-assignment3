6b99a6eb1dae841d8f81109b6a4620c38a48ef0c
JFingert <joshuafingert@gmail.com>
2013-10-12 13:11:01 -0700

    changes

--- Fingert/f2c-fingert.js
+++ Fingert/f2c-fingert.js
@@ -16,16 +16,24 @@ console.log("Water boils at " + celsiusToFahrenheit(100) + " degrees Fahrenheit.
    this case) are "hoisted" to the top of the program at runtime. This means we
    can make a convention of defining our functions at the end of current scope. */
 
+<<<<<<< HEAD
 function celsiusToFahrenheit () {
 
   return (temperature * 9) / 5 + 32;
   // the problem here is that 'temperature' is a global variable
   // you need to be passing in a parameter value to convert
   // instead of referencing 'temperature' within the function
+=======
+function celsiusToFahrenheit (x) {
+  // You should not use a global variable for the function return value
+  // You don't really need a variable at all here. Just return the value of the expression.
+  return (x * 9) / 5 + 32;
+  
+>>>>>>> changes
 }
 
-function fahrenheitToCelsius () {
+function fahrenheitToCelsius (x) {
   // Same comments as above
-  return ((temperature - 32) * 5) / 9;
+  return ((x - 32) * 5) / 9;
   
 }
