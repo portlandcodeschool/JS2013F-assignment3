429a3cd3611f19a73f111b3c41eb923eaf249b67
grumancik <genrumancik@gmail.com>
2013-10-15 16:07:49 -0700

    changes

--- Rumancik/wordreverse.js
+++ Rumancik/wordreverse.js
@@ -1,20 +1,15 @@
 
-
-/*function Reverse (cow) {
-var x = ("Let's do some stuff.");
-var cow = x.split(" ").reverse().join(" ")
-for (var i = 3; i < -1; i -= 1)
- return(Reverse)
-}
-console.log(Reverse)*/
-
 function Reverse (cow) {
 	var str = ""
 	var cowArray = cow.split(" ").reverse();
 	for(var i=0;i<cowArray.length;i++)
 	{
-		str = str + cowArray[i]
+		str = str + cowArray[i] 
+		if (i !== cowArray.length -1) {
+			str = str + " "
+		}
 	}
 	return str 
 };
 console.log(Reverse("one two three four"))
+console.log(Reverse("The cow goes moo"))
