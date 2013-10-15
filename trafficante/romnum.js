dd6ce104ebd214cf6a2c7ffd971e0ee0b96fab76
Austin Trafficante <austin.trafficante@gmail.com>
2013-10-15 15:17:14 -0700

    Finished f2c

--- trafficante/romnum.js
+++ trafficante/romnum.js
@@ -32,4 +32,10 @@ function romanToDecimal (romNumeral) {
 module.exports.decimalToRoman = decimalToRoman;
 module.exports.romanToDecimal = romanToDecimal;
 
-for commit
+/*
+####romnum.js Directions
+
+This assignment is significantly more challenging. You need to implement two functions. One will convert a decimal number (Number type) to a roman numeral string. The other will convert a roman numeral string to a decimal number (Number type).
+
+Pay close attention to the comments in the file about where to put things. I've included some code for me to use to test your work. You shouldn't need to change it.
+*/
