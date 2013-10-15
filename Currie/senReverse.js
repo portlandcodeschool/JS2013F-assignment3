458407af5ab4cf4f62b5319ec8afbcec4793d90c
curriedav <curriedav@gmail.com>
2013-10-14 20:01:33 -0700

    adds senReverse function

---
+++ Currie/senReverse.js
@@ -0,0 +1,10 @@
+
+var string = "I have a funny-looking dog named Koby-Beef.";
+
+function reverse(s){
+    return s.split(" ").reverse().join(" ");
+};
+
+reverse(string);
+
+console.log(reverse(string));
