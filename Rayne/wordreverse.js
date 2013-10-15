f3516a772fee2a379182f5525dbad9cef3963130
kaijarayne <kaijarayne@gmail.com>
2013-10-15 12:45:24 -0700

    Adding wordreverse challenge

---
+++ Rayne/wordreverse.js
@@ -0,0 +1,8 @@
+var wordReverse = function(inputSentence) {
+	return inputSentence.split(" ").reverse().join(" ")+".";
+};
+
+console.log(wordReverse("I am the very model of a modern Major General"));
+
+
+
