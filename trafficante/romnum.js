5a7b2213d735f2707d3ff776187513513ffec937
Austin Trafficante <austin.trafficante@gmail.com>
2013-10-15 18:14:31 -0700

    finishes first half of romnum.js

--- trafficante/romnum.js
+++ trafficante/romnum.js
@@ -1,7 +1,7 @@
 if(!module.parent) { // Put any code you need to test your functions in here:
 
 // Example:
-console.log("53 in roman numerals is: " + decimalToRoman(53));  
+console.log(decimalToRoman(1479));  
 
 }
 /*-----------------------------------------------------------------------------
@@ -14,15 +14,94 @@ console.log("53 in roman numerals is: " + decimalToRoman(53));
 function decimalToRoman (decNumber) {
   var romNumeral = "";
 
-  // Implement here!
+  var romThousands = Math.floor(decNumber / 1000);
+  var romHundreds = Math.floor(decNumber % 1000 / 100);
+  var romTens = Math.floor(decNumber % 100 / 10);
+  var romOnes = Math.floor(decNumber % 10 / 1);
 
-  return romNumeral;
+  while(romThousands--) {
+  	romNumeral += "M";
+  }
+
+  if (romHundreds == 4) {
+  	romNumeral += "CD";
+  }
+  else if (romHundreds == 5) {
+  	romNumeral += "D";
+  }
+  else if (romHundreds == 6) {
+  	romNumeral += "DC";
+  }
+  else if (romHundreds == 7) {
+  	romNumeral += "DCC";
+  }
+  else if (romHundreds == 8) {
+  	romNumeral += "DCCC"
+  }
+  else if (romHundreds == 9) {
+  	romNumeral += "CM";
+  }
+  else {
+  	while(romHundreds--) {
+  		romNumeral += "C";
+  	}
+  }
+  
+  if (romTens == 4) {
+  	romNumeral += "XL";
+  }
+  else if (romTens == 5) {
+  	romNumeral += "L";
+  }
+  else if (romTens == 6) {
+  	romNumeral += "LX";
+  }
+  else if (romTens == 7) {
+  	romNumeral += "LXX";
+  }
+  else if (romTens == 8) {
+  	romNumeral += "LXXX";
+  }
+  else if (romTens == 9) {
+  	romNumeral += "XC"
+  }
+  else {
+  	while(romTens--) {
+  		romNumeral += "X";
+  	}
+  }
+ 
+  if (romOnes == 4) {
+  	romNumeral += "IV";
+	}
+	else if (romOnes == 5) {
+		romNumeral += "V";
+	}
+	else if (romOnes == 6) {
+		romNumeral += "VI";
+	}
+	else if (romOnes == 7) {
+		romNumeral += "VII";
+	}
+	else if (romOnes == 8) {
+		romNumeral += "VIII";
+	}
+	else if (romOnes == 9) {
+		romNumeral += "IX";
+	}
+  else {
+	  while(romOnes--) {
+  		romNumeral += "I";
+  	}
+  }
+
+  return romNumeral
 }
 
 function romanToDecimal (romNumeral) {
   var decNumber = 0;
 
-  // Implement here!
+  
 
   return decNumber;
 }
