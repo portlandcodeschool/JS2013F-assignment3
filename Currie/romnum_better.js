0d156bfc5b2d3f104dcfefb9e1c15eb655c45b7d
curriedav <curriedav@gmail.com>
2013-10-15 18:02:48 -0700

    edits to romnum_better

--- Currie/romnum_better.js
+++ Currie/romnum_better.js
@@ -1,11 +1,11 @@
-//http://www.iandevlin.com/blog/2010/03/javascript/converting-decimal-numbers-to-roman-numerals-in-javascript
-
-
+//SOURCE: http://www.iandevlin.com/blog/2010/03/javascript/converting-decimal-numbers-to-roman-numerals-in-javascript
 
 var roman = new Array(); 
 roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]; 
+
 var decimal = new Array(); 
 decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1]; 
+
 function decimalToRomanSimple(value) { 
   if (value <= 0 || value >= 4000) return value; 
     var romanNumeral = ""; 
@@ -35,18 +35,24 @@ function getRoman(value) {
 function decimalToRoman(value) { 
   // 3,888,888 is the longest number represented by Roman numerals 
   if (value <= 0 || value > 3888888) return value; 
+  
   var romanNumeral1 = ""; 
   var romanO = getRoman(value); 
+  
   // If the number is 4000 or greater 
   if (romanO.numThousands > 4) { 
+  
     var thousandString = ""; 
+  
     for (var j=0;j<romanO.numThousands;j++) thousandString += "M"; 
       var thousandsO = getRoman(romanO.numThousands); 
       var thBase = "<span style='border-top:1px solid #000'>" +  
 thousandsO.romanNumeral + "</span>"; 
       romanNumeral = romanO.romanNumeral.replace(thousandString, thBase); 
     } 
+  
     else romanNumeral = romanO.romanNumeral; 
+  
     return romanNumeral; 
 }
 
