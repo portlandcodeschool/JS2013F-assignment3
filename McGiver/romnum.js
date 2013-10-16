be5fb2ef3f4fbff51932c00a22c530394bc6b2be
garbagemanonfire <nilesvm@gmail.com>
2013-10-15 18:14:44 -0700

    Updated Romunnumeral problem

--- McGiver/romnum.js
+++ McGiver/romnum.js
@@ -1,3 +1,5 @@
+
+
 //if(!module.parent) { // Put any code you need to test your functions in here:
 //
 //// Example:
@@ -6,6 +8,7 @@
 //}
 //
 
+
 /*-----------------------------------------------------------------------------
      
 
@@ -13,37 +16,124 @@
 
 
 -----------------------------------------------------------------------------*/
-
-
-function romanize (53) {
-    if (!+num)
-        return false;
-    var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "","I","II","III","IV","V","VI","VII","VIII","IX"],
-        roman = "",
-        i = 3;
-    while (i--)
-        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
+function decimalToRoman (1053) {
+  	var romNumeral = "";
+    while (i >= 1000) {
+        romNumeral += "M";
+        i -= 1000;        }
+    while (i >= 900) {
+        romNumeral += "CM";
+        i -= 900;
+    }
+    while (i >= 500) {
+        romNumeral += "D";
+        i -= 500;
+    }
+    while (i >= 400) {
+        romNumeral += "CD";
+        i -= 400;
+    }
+    while (i >= 100) {
+        romNumeral += "C";
+        i -= 100;
+    }
+    while (i >= 90) {
+        romNumeral += "XC";
+        i -= 90;
+    }
+    while (i >= 50) {
+        romNumeral += "L";
+        i -= 50;
+    }
+    while (i >= 40) {
+        romNumeral += "XL";
+        i -= 40;
+    }
+    while (i >= 10) {
+        romNumeral += "X";
+        i -= 10;
+    }
+    while (i >= 9) {
+        romNumeral += "IX";
+        i -= 9;
+    }
+    while (i >= 5) {
+        romNumeral += "V";
+        i -= 5;
+    }
+    while (i >= 4) {
+        romNumeral += "IV";
+        i -= 4;
+    }
+    while (i >= 1) {
+        romNumeral += "I";
+        i -= 1;
+    }    
+    console.log(romNumeral);
+	return romNumeral;
 }
 
 
-//
-//function decimalToRoman (decNumber) {
-//  var romNumeral = "";
-//
-//  return romNumeral;
-//}
-//
-//function romanToDecimal (romNumeral) {
-//  var decNumber = 0;
-//
-//  // Implement here!
-//
-//  return decNumber;
-//}
+
+function romanToDecimal (i) {
+  	var decNumber = 0;
+  	while (i.contains "M") {
+        romNumeral += "1000";
+        i -= "m";        }
+//    while (i >= 900) {
+//        romNumeral += "CM";
+//        i -= 900;
+//    }
+//    while (i >= 500) {
+//        romNumeral += "D";
+//        i -= 500;
+//    }
+//    while (i >= 400) {
+//        romNumeral += "CD";
+//        i -= 400;
+//    }
+//    while (i >= 100) {
+//        romNumeral += "C";
+//        i -= 100;
+//    }
+//    while (i >= 90) {
+//        romNumeral += "XC";
+//        i -= 90;
+//    }
+//    while (i >= 50) {
+//        romNumeral += "L";
+//        i -= 50;
+//    }
+//    while (i >= 40) {
+//        romNumeral += "XL";
+//        i -= 40;
+//    }
+//    while (i >= 10) {
+//        romNumeral += "X";
+//        i -= 10;
+//    }
+//    while (i >= 9) {
+//        romNumeral += "IX";
+//        i -= 9;
+//    }
+//    while (i >= 5) {
+//        romNumeral += "V";
+//        i -= 5;
+//    }
+//    while (i >= 4) {
+//        romNumeral += "IV";
+//        i -= 4;
+//    }
+//    while (i >= 1) {
+//        romNumeral += "I";
+//        i -= 1;
+//    }    
+  console.log(decNumber);
+  return decNumber;
+}
+
+decimalToRoman(1234)
+romantoDecimal(M)
 
 // Don't worry about this stuff. It's here so I can more easily test your code.
 
